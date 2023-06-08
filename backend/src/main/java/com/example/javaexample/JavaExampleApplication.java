9cc70074707efb37cece0fcd7d0fc22fc1cc7316
ambiandji <112909914+ambiandji@users.noreply.github.com>
2023-06-08 13:09:54 +0200

    added a new line (#1)
    * CI Workflow
    ---------
    
    Co-authored-by: ambiandji <ambiandji@gmail.com>

--- backend/src/main/java/com/example/javaexample/JavaExampleApplication.java
+++ backend/src/main/java/com/example/javaexample/JavaExampleApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaExampleApplication.class, args);
+
     }
 
 }
